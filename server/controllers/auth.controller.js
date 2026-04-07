4dce25f1946fc4fb2be35acd2815ef13ccd4296c
rohitanshu95 <rohitanshudhar86@gmail.com>
2026-04-07 16:34:23 +0530

    cookies set

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const login = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: " lax",
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
 
